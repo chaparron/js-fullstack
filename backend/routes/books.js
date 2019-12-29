fb2203d211cab4feb4377167d806eec3a3a7f7b4
chaparron <chaparron@github.com>
2019-12-29 20:53:14 +0100

    #7 configuramos la rest api a través de postman

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,6 +1,23 @@
 const {Router} = require ('express');
 const router = Router();
 
-router.get('/', (req,res) => res.json({text: 'Hello world'}));
+const Book = require('../models/book');
+
+router.get('/', async(req,res) => {
+    const books = await Book.find();
+    res.json(books);
+});
+
+router.post('/', async(req,res) =>{
+    const {title, author, isbn} = req.body;
+    const newBook = new Book({title, author, isbn});
+    await newBook.save();
+    res.json({message: 'book saved'})
+});
+
+router.delete('/:id', async(req,res)=>{
+    const book = await Book.findByIdAndDelete(req.params.id);
+    res.json({message: 'libro eliminado'});
+});
 
 module.exports = router;
